1a302252307a7f4abce507c8b52fa86a50e78072
akhila-nori <akhilanori2001@gmail.com>
2025-12-14 16:54:12 +0530

    multiple query param being passed

--- src/main/java/com/module2/module2/controllers/Employee.java
+++ src/main/java/com/module2/module2/controllers/Employee.java
@@ -3,15 +3,22 @@ package com.module2.module2.controllers;
 import com.module2.module2.dto.EmployeeDTO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 public class Employee {
     @GetMapping("/employees/{employeId}")
     public EmployeeDTO getEmployeeById(@PathVariable Long employeId){
         return new EmployeeDTO(employeId,"Akhila","akhila@gamil.com",24, LocalDate.of(2025,1,2),true);
+    }
+
+    @GetMapping("/employees")
+    public String getAllEmployees(@RequestParam(required=false) Integer age, @RequestParam(required=false) String sortBy){
+        return "Hi age is ..."+age + " " +sortBy;
 
     }
 
