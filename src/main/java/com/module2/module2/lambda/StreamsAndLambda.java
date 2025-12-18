1d4cfe6864b9b0284b14cbe5c88d8842bc1f0e71
akhila-nori <akhilanori2001@gmail.com>
2025-12-18 17:23:05 +0530

    Streams and Collector used groupingBy to group the fruits by the length of the fruits

--- src/main/java/com/module2/module2/lambda/StreamsAndLambda.java
+++ src/main/java/com/module2/module2/lambda/StreamsAndLambda.java
@@ -28,7 +28,7 @@ public class StreamsAndLambda {
 
 
         //convert List<> into Stream and perform stream related operations on this converted stream
-        List<String> fruits = List.of("Apple", "Kiwi", "Banana");
+        List<String> fruits = List.of("Apple", "Kiwi", "Banana","Mango");
 
         Stream<String> stream = fruits.stream();
 
@@ -48,14 +48,54 @@ public class StreamsAndLambda {
 //    }
 
         //METHOD 3
-        Map<String, Integer> frui = fruits
+//        Map<String, Integer> frui = fruits
+                Map<Integer, Long> fr = fruits
                 .stream()
 //                .map(f -> f.length())
 //                .collect(Collectors.toMap(fr -> fr, fruVal -> fruVal.length() ));  //lambda version
-                  .collect(Collectors.toMap(fr -> fr, String:: length)); //method referencing version
-        //It tells Java: "I have a String here; please just run its .length() method and give me the result."
 
-        System.out.println("fruits using collector ..." + frui);
+
+//                  .collect(Collectors.toMap(fr -> fr, String:: length)); //method referencing version ,//It tells Java: "I have a String here; please just run its .length() method and give me the result."
+
+
+                  .collect(Collectors.groupingBy(
+                          String::length,
+                          Collectors.counting()
+                  ));
+
+
+                //Method - 4 main logic ********************************
+                Map<Integer,List<String>> result = fruits
+                        .stream()
+                        .collect(Collectors.groupingBy(String::length));
+                //How to find the KEY
+        //Look at the Classifier (the first argument inside the parentheses).
+        //
+        //Code: String::length --> length() of String calculation --> method referencing
+        //
+        //Rule: Whatever this function returns becomes your Key.
+        //
+        //Logic: Since .length() returns a number, your Key is an Integer.
+
+
+//        How to find the VALUE
+//        Look at the Source of the Stream and the Arguments.
+//
+//        Rule A (Default): If there is only one argument in groupingBy, the Value is always a List of the objects currently in the stream.
+//
+//        Source: You started with fruits.stream(), which is a stream of String.
+//
+//                Logic: Therefore, your Value is List<String>.
+
+
+
+
+
+                //Look at each item: It takes every String in your stream (e.g., "Apple", "Kiwi").Calculate the "Label" (The Key): It runs String::length on each item."Apple" $\rightarrow$ 5"Kiwi" $\rightarrow$ 4"Banana" $\rightarrow$ 6"Mango" $\rightarrow$ 5
+       //Group them into Lists (The Value): It creates a Map where the Keys are the lengths <Integer>, and the Values are Lists containing all strings that have that length List<String>
+//The Resulting Map: { 4=["Kiwi"], 5=["Apple", "Mango"], 6=["Banana"] }
+
+        System.out.println("fruits using collector ..." + result);
     }
 
 
