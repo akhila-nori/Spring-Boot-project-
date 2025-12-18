6694ef59b015194f8aa092b57148e0c383376e83
akhila-nori <akhilanori2001@gmail.com>
2025-12-18 16:53:53 +0530

    Streams and Collector used

--- src/main/java/com/module2/module2/lambda/StreamsAndLambda.java
+++ src/main/java/com/module2/module2/lambda/StreamsAndLambda.java
@@ -1,6 +1,8 @@
 package com.module2.module2.lambda;
 
+import java.sql.SQLOutput;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.hibernate.internal.util.collections.ArrayHelper.forEach;
@@ -15,32 +17,45 @@ public class StreamsAndLambda {
         //lambda expression introduced in java 8
         Walkable obj = (steps, isEnabled) -> {
 
-            System.out.println("Walking fast "+steps + " steps.");
-            return 2*steps;
+            System.out.println("Walking fast " + steps + " steps.");
+            return 2 * steps;
         };
 
-        Walkable obj2 = (steps, isEnabled) -> 2*steps; //returning , 1 line of body, avoiding curly braces
+        Walkable obj2 = (steps, isEnabled) -> 2 * steps; //returning , 1 line of body, avoiding curly braces
 
-        obj.walk(4,true);
+        obj.walk(4, true);
 
 
         //convert List<> into Stream and perform stream related operations on this converted stream
-        List<String> fruits = List.of("Apple","Kiwi","Banana");
+        List<String> fruits = List.of("Apple", "Kiwi", "Banana");
 
         Stream<String> stream = fruits.stream();
 
+        //METHOD - 1
 //        stream.forEach((f) -> {
 //            System.out.println(f);
 //        });
 
-        stream
-                .filter(frui -> frui.length() < 5)
-                .sorted()
-                .map(f -> f.length())
-                .map(fruitLegnth -> 2*fruitLegnth)
-                .forEach(fr -> System.out.println(fr));
+        //METHOD -2
+//        stream
+//                .filter(frui -> frui.length() < 5)
+//                .sorted()
+//                .map(f -> f.length()) //getting this stream
+//                .map(fruitLegnth -> 2*fruitLegnth)
+//                .forEach(fr -> System.out.println(fr));
+//
+//    }
 
+        //METHOD 3
+        List<Integer> frui = fruits
+                .stream()
+                .map(f -> f.length())
+                .collect(Collectors.toList());
+
+        System.out.println("fruits using collector ..." + frui);
     }
+
+
 }
 
 @FunctionalInterface
