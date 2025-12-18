5e87da08a3666bce175de16afb6f20ec81d7e6e4
akhila-nori <akhilanori2001@gmail.com>
2025-12-18 16:09:53 +0530

    Lambda Expression with functional interfaces

--- src/main/java/com/module2/module2/lambda/StreamsAndLambda.java
+++ src/main/java/com/module2/module2/lambda/StreamsAndLambda.java
@@ -2,20 +2,35 @@ package com.module2.module2.lambda;
 
 public class StreamsAndLambda {
     public static void main(String[] args) {
-        Walkable w = new Walkfast();
-        w.walk(5);
+
+        //Older way of dealing
+//        Walkable w = new Walkfast();
+//        w.walk(5);
+
+        //lambda expression introduced in java 8
+        Walkable obj = (steps, isEnabled) -> {
+
+            System.out.println("Walking fast "+steps + " steps.");
+            return 2*steps;
+        };
+
+        Walkable obj2 = (steps, isEnabled) -> 2*steps; //returning , 1 line of body, avoiding curly braces
+
+        obj.walk(4,true);
+
     }
 }
 
+@FunctionalInterface
 interface Walkable {
-    int walk(int steps);
+    int walk(int steps, boolean isEnabled);
 }
 
-class Walkfast implements Walkable {
-
-    @Override
-    public int walk(int steps) {
-        System.out.println("Walking fast "+steps + " steps.");
-        return 2*steps;
-    }
-}
+//class Walkfast implements Walkable {
+//
+//    @Override
+//    public int walk(int steps) {
+//        System.out.println("Walking fast "+steps + " steps.");
+//        return 2*steps;
+//    }
+//}
